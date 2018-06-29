bc477e90043243f2e963f26567e323ef877a479a
Gagandeep Kaur <gagan.cheema@gmail.com>
2018-06-30 01:19:03 +1000

    fixed script

--- upload.php
+++ upload.php
@@ -21,13 +21,10 @@
 	<div class="header"></div>
 		
 	<div class="main">
+		<form enctype="multipart/form-data" action="upload.php" method="POST">
 
 		<div class="item">
 
-			<div class="date">
-				<div>OCT</div>
-				<span>01</span>
-			</div>
 
 			<div class="content">
 
@@ -81,13 +78,13 @@
 
 			<div class="content">
 
-				<h1>Choose button type</h1>
+				<h1>Upload</h1>
 
 				<div class="body">
 		
 					<p>Upload file</p>
-					<input type="text" name="fileName"></input> <br/>
-					<input type="submit" value="Upload"></input>
+					<input type="file" name="fileName"></input> <br/>
+					<input type="submit" value="submit"></input>
     
 				</div>
 
@@ -95,7 +92,7 @@
 
 		</div>
 
-
+		</form>
 	</div>
 
 	<div class="navigation">
@@ -124,7 +121,7 @@
 			<li><a href="index.html">consequat molestie</a></li>
 		</ul>
 
-		  <form enctype="multipart/form-data" action="upload.php" method="POST">
+		  
 
 	</div>
 	
@@ -148,44 +145,48 @@
   print_r($a);
 */
 
-
-if($_POST['submit']) {
-
+if(isset($_POST['button'])){
+	$jsonfile = "amazon-dash.json";
     $selectedBtn = $_POST['button'];
     $selectedType = $_POST['buttonType'];
-    $string = file_get_contents("amazon-dash.json");
+    $string = file_get_contents($jsonfile);
     $json_a = json_decode($string, true);
-
-    print_r ($json_a);
-    if(!empty($_FILES['uploaded_file']))
+    
+    if(!empty($_FILES['fileName']))
       {
 
         $path = "uploads/";
-        $ext = pathinfo($_FILES['uploaded_file']['name'], PATHINFO_EXTENSION);
+        $ext = pathinfo($_FILES['fileName']['name'], PATHINFO_EXTENSION);
         $newName = $selectedBtn.".".$ext;
         $path = $path.$newName;
 
-        if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+        if(move_uploaded_file($_FILES['fileName']['tmp_name'], $path)) {
           echo "The file ".  $newName. " has been uploaded";
         } else{
             echo "There was an error uploading the file, please try again!";
         }
       }
 
+	$newJson = array();
     foreach($json_a as $key => $value)
     {
+	  $newJson[$key] = $value;
       if ($key == 'devices')
       {
             foreach($value as $mac => $detail) {
-                if($detail['name'] == $selectedBtn) {
-                        $cmd = if($selectedType == 'sound')? "omxplayer ".$path."  --vol 1000" : 'curl -X POST https://maker.ifttt.com/trigger/dash_button_press/with/key/felqP2zXnKwE5LsqkLXZf7koMVNYDP9P2L_k4iXWQkJ';
-                        $detail['cmd'] = $cmd;
+                if(trim($detail['name']) == trim($selectedBtn)) {
+                       $cmd = ($selectedType == 'sound')? "omxplayer /".$path."  --vol 1000" : 'curl -X POST https://maker.ifttt.com/trigger/dash_button_press/with/key/felqP2zXnKwE5LsqkLXZf7koMVNYDP9P2L_k4iXWQkJ';
+                        //$detail['cmd'] = $cmd;
+						$newJson[$key][$mac]['cmd'] = $cmd;
                 }
             }
             
       }
     }
-     echo $updatedJson = json_encode($json_a);
+	$fp = fopen($jsonfile, 'w');
+	fwrite($fp, json_encode($newJson));
+	fclose($fp);
+
     
    
 }
