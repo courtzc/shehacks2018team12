cbc3c016cf26b1eed6b96f8548b076c7934d7016
JessFlan <JessFlan@users.noreply.github.com>
2018-06-30 14:54:48 +1000

    fixed json and yml locations

--- update_config.sh
+++ update_config.sh
@@ -1,2 +1,2 @@
-sudo -S json2yaml  /var/www/html/amazon-dash.json  /home/pi/dash-python/shehacks2018team12/amazon-dash.yml
+sudo -S json2yaml  /var/www/html/shehacks2018team12/amazon-dash.json  /home/pi/dash-python/amazon-dash/amazon-dash.yml
 sudo -S systemctl restart amazon-dash
